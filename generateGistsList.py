9d238adc43a8bca6b14e0e9c6afb536a1bc6f4dc
Forgo7ten <forgo7ten2020@gmail.com>
2021-11-28 16:59:46 +0800

    update py

--- generateGistsList.py
+++ generateGistsList.py
@@ -9,7 +9,7 @@ import time
 import requests
 
 gistUrl = "https://gist.github.com/Forgo7ten"
-my_head = {
+my_headers = {
     "User-Agent":
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36"
 }
@@ -35,7 +35,7 @@ def add_info(f):
 
 
 def get_gist_info(f):
-    r = requests.get(gistUrl, headers=my_head)
+    r = requests.get(gistUrl, headers=my_headers)
     gists = "fail"
     if 200 == r.status_code:
         gists = partten.findall(r.text.replace("\n", " "))
